84431469f06ef4ec5b95e95ecf0d04608f690b35
Gisela Soledad Hurtado <hurtadogiselasoledad@gmail.com>
2024-08-23 18:08:41 +0200

    lastest changes

--- src/test/java/dev/gisela/CheckingAccountTest.java
+++ src/test/java/dev/gisela/CheckingAccountTest.java
@@ -14,6 +14,14 @@ class CheckingAccountTest {
     }
 
     @Test
+    void testWithdrawWithoutOverdraft() {
+        CheckingAccount account = new CheckingAccount(1000, 0.05f);
+        account.withdraw(500);
+        assertEquals(500, account.balance);
+        assertEquals(0, account.overdraft);
+    }
+
+    @Test
     void testDeposit() {
         CheckingAccount account = new CheckingAccount(1000, 0.05f);
         account.overdraft = 500;
@@ -23,10 +31,54 @@ class CheckingAccountTest {
     }
 
     @Test
+    void testDepositCoveringOverdraft() {
+        CheckingAccount account = new CheckingAccount(0, 0.05f);
+        account.overdraft = 500;
+        account.deposit(600);
+        assertEquals(0.0, account.balance);
+        assertEquals(100, account.overdraft);
+    }
+
+    @Test
+    void testDepositWithoutOverdraft() {
+        CheckingAccount account = new CheckingAccount(0, 0.05f);
+        account.deposit(1000);
+        assertEquals(1000, account.balance);
+        assertEquals(0, account.overdraft);
+    }
+
+    @Test
     void testMonthlyStatement() {
         CheckingAccount account = new CheckingAccount(1000, 0.05f);
         account.monthlyFee = 50;
         account.monthlyStatement();
         assertEquals(953.95, account.balance, 0.01);
+    }
+
+  
+
+    @Test
+    void testPrint() {
+        CheckingAccount account = new CheckingAccount(1000, 0.05f);
+        account.withdraw(1500);
+        account.print();
+       
+    }
+
+    @Test
+    void testDepositCoversPartOfOverdraft() {
+        CheckingAccount account = new CheckingAccount(0, 0.05f);
+        account.overdraft = 600;
+        account.deposit(500);
+        assertEquals(100, account.balance);
+        assertEquals(0.0, account.overdraft);
+    }
+
+    @Test
+    void testDepositNegativeAmount() {
+        CheckingAccount account = new CheckingAccount(0, 0.05f);
+        account.deposit(0);
+        assertEquals(0, account.balance); 
+        assertEquals(0, account.overdraft); 
     }
 }
